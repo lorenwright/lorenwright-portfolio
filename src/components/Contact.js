c994964e3c6cef1a44774e26240471d5c71fff9a
developers@barsnesssolutions.com <developers@fjorgedigital.com>
2018-04-26 22:50:49 -0500

    Update portfolio links and minor content updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import data from '../controllers/Data.json';
 class Contact extends React.Component {
     render() {
         return (
-            <a href="mailto:lorenwrightdesign@gmail.com" className="contact-button">
+            <a href="mailto:loren.wright.dev@gmail.com" className="contact-button">
                 <div className="contact">
                     <div className="container">
                         <div className="contact-text"><i className="fa fa-envelope-o"></i>Like what you see? Hire me!</div>
